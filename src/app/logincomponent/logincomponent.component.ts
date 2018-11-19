3f60df750a4efdf112b443f251b98321bb9805f5
albin_paul <albin.paul@flytxt.com>
2018-11-19 17:12:05 +0530

    sdf

--- src/app/logincomponent/logincomponent.component.ts
+++ src/app/logincomponent/logincomponent.component.ts
@@ -4,26 +4,12 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
   selector: 'app-logincomponent',
   // templateUrl: './logincomponent.component.html',
   template: `
-  {{profileForm.value | json}}
-  <form [formGroup]="profileForm" (ngSubmit)="onSubmit()">
-  <input #user formControlName='username'type="text"/><br/>
-  <input #user1 formControlName='username'type="text"/><br/>
-  <input #password formControlName='password' type="password"/>
-  <br/>
-  <button type="submit" [disabled]="!profileForm.valid">Submit</button>
-  </form>
+
   `,
   styleUrls: ['./logincomponent.component.sass']
 })
 export class LogincomponentComponent implements OnInit {
-  profileForm = new FormGroup({
-    username : new FormControl(''),
-    password : new FormControl(''),
-  } ) ;
   constructor() {}
   ngOnInit() {
-  }
-  onSubmit() {
-    console.warn(this.profileForm.value);
   }
 }
