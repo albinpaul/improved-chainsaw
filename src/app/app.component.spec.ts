3f9cff7d6c2d36d20dcae9680d3c2072885e1add
albin_paul <albin.paul@flytxt.com>
2018-11-19 14:30:44 +0530

    changed project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'improved-chainsaw'`, () => {
+  it(`should have as title 'bingo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('improved-chainsaw');
+    expect(app.title).toEqual('bingo');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to improved-chainsaw!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bingo!');
   });
 });
