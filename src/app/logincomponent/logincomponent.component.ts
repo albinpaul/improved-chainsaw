6e45b4c1a488f63202a644ea6a0dd1a5c39f1ed9
albin_paul <albin.paul@flytxt.com>
2018-11-19 17:57:17 +0530

    changes added

--- src/app/logincomponent/logincomponent.component.ts
+++ src/app/logincomponent/logincomponent.component.ts
@@ -4,12 +4,33 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
   selector: 'app-logincomponent',
   // templateUrl: './logincomponent.component.html',
   template: `
-
+  <span *ngFor="let hero of Heros;index as i" >{{i%5 == 0}} {{hero}}
+  </span>
+  <br *ngIf="i%5 == 4"/>
   `,
   styleUrls: ['./logincomponent.component.sass']
 })
+
+
+
 export class LogincomponentComponent implements OnInit {
-  constructor() {}
+  Heros: string[] = [];
+  constructor() { }
+  shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
   ngOnInit() {
+    for ( let i = 1 ; i <= 25 ; i++ ) {
+      this.Heros.push(i.toString());
+    }
+    this.Heros = this.shuffle(this.Heros);
   }
 }
